1cde292f159b1f91f3e7664902dfb7553c8c1923
merceprog <jeffersongonzaga09@gmail.com>
2017-02-24 20:35:06 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+  $con = mysql_connect("localhost,id902836_mercenary,02242017,id902836_mercenary");
+  
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  
+  $statement = mysqli_prepare($con, "SELECT * FROM user WHERE email = ? AND password = ?);
+  mysqli_stmt_bind_param($statement, "ss", $email, $password);
+  mysqli_stmt_execute($statement);
+  
+  mysqli_stmt_store_result($statement(;
+  mysqli_stmt_blind_result($statement, $userID, $firstname, $lastname, $email, $password );
+  
+  $response = array();
+  $response["success"] = false;
+  
+  while(mysqli_stmt_fetch($statement)){
+    $response["success"] = true;
+    $response["firstname"] = $firstname;
+    $response["lastname"] = $lastname;
+    $response["email"] = $email;
+    $response["password"] = $password;
+  }
+  
+  echo json_encode($response);
+?>
