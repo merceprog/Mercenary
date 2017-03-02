ad31073d960049b7434c2957461c20c2907a7565
merceprog <jeffersongonzaga09@gmail.com>
2017-03-02 17:57:23 +0800

    Create Register.php

---
+++ Register.php
@@ -0,0 +1,19 @@
+<?php
+  $con = mysql_connect("localhost,id902836_mercenary,02242017,id902836_mercenary");
+  
+  $firstname = $_POST["firstname"];
+  $lastname = $_POST[lastname];
+  $contact = $_POST["contact"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  
+  
+  $statement = mysqli_prepare($con, "INSERT INTO user (firstname, lastname, contact, email, password) VALUES (?, ?, ?, ?, ?)";
+  mysqli_stmt_bind_param($statement, "ssiss", $firstname, lastname, contact, $email, $password);
+  mysqli_stmt_execute($statement);
+  
+  $response = array();
+  $response["success"] = true;
+  
+  echo json_encode($response);
+?>
